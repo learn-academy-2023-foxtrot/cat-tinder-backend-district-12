27baccf9578e6ea283db6a2c24575b732552d6d1
XenaSit <sitnikovaxena@gmail.com>
2023-10-17 15:48:56 -0700

    copleted validations

--- app/models/tribute.rb
+++ app/models/tribute.rb
@@ -1,2 +1,4 @@
 class Tribute < ApplicationRecord
+    validates :name, :age, :hobby, :image, :district,  presence: true
+    validates :hobby, length: { minimum:10 } 
 end
--- spec/models/tribute_spec.rb
+++ spec/models/tribute_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Tribute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should validate name' do
+    tribute = Tribute.create(
+      age: 16, 
+      hobby: 'shooting up birds and squirrels', 
+      image: 'https://live.staticflickr.com/606/21731063948_c2bdd0aef6_b.jpg', 
+      district: 'District 12'
+    )
+    expect(tribute.errors[:name]).to_not be_empty
+  end
+  it 'should validate age' do
+    tribute = Tribute.create(
+      name: 'Katniss Everdeen',
+      age: nil, 
+      hobby: 'shooting up birds and squirrels', 
+      image: 'https://live.staticflickr.com/606/21731063948_c2bdd0aef6_b.jpg', 
+      district: 'District 12'
+    )
+    expect(tribute.errors[:age]).to_not be_empty
+  end
+  it 'should return length of hobby' do
+    tribute = Tribute.create(
+      name: 'Katniss Everdeen',
+      age: 16, 
+      hobby: 'shooting', 
+      image: 'https://live.staticflickr.com/606/21731063948_c2bdd0aef6_b.jpg', 
+      district: 'District 12'
+    )
+    expect(tribute.errors[:hobby]).to_not be_empty
+  end
+  it 'should return image' do
+    tribute = Tribute.create(
+      name: 'Katniss Everdeen',
+      age: 16, 
+      hobby: 'shooting', 
+      image: nil, 
+      district: 'District 12'
+    )
+    expect(tribute.errors[:image]).to_not be_empty
+  end
+  it 'should return district' do
+    tribute = Tribute.create(
+      name: 'Katniss Everdeen',
+      age: 16, 
+      hobby: 'shooting', 
+      image: 'https://live.staticflickr.com/606/21731063948_c2bdd0aef6_b.jpg', 
+      district: nil
+    )
+    expect(tribute.errors[:district]).to_not be_empty
+  end
 end
