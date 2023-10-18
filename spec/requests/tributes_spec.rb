9d83417dcc2d55e985932e23c1295f19830a07e6
XenaSit <sitnikovaxena@gmail.com>
2023-10-17 17:13:23 -0700

    finnished validations

--- spec/requests/tributes_spec.rb
+++ spec/requests/tributes_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "Tributes", type: :request do
     end
     describe "POST /create" do
       it "creates a tribute" do 
-        tribute_params = {
+        tribute_param = {
           tribute: {
             name: 'Katniss Everdeen', 
             age: 16, 
@@ -34,6 +34,21 @@ RSpec.describe "Tributes", type: :request do
         expect(response).to have_http_status(200)
         tribute = Tribute.first
         expect(tribute.name).to eq 'Katniss Everdeen'
+      end
+      it 'creates a new tribute' do
+        tribute_params = {
+          tribute: {
+            name: nil,
+            age: 16, 
+            hobby: 'shooting', 
+            image: 'https://live.staticflickr.com/606/21731063948_c2bdd0aef6_b.jpg', 
+            district: 'District 12'
+          }
+        }
+        post '/tribute', params: tribute_params
+        expect(response.status).to eq 422
+        tribute_error = JSON.parse(response.body)
+        expect(tribute_error['name']).to include "can't be blank"
       end
     end
   end
